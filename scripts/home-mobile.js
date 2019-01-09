bc91fdc91dca55546e013a3ec92f174afe7eabc3
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-01-09 21:43:18 +0700

    commit

--- scripts/home-mobile.js
+++ scripts/home-mobile.js
@@ -1,2 +1,26 @@
+var menuOpened = false;
+
 $(document).ready(function() {
 });
+
+function openMenu() {
+    if (!menuOpened) {
+        $("#menu").animate({
+            left: "0"
+        }, {
+            duration: 300,
+            complete: function() {
+                menuOpened = true;
+            }
+        });
+    } else {
+        $("#menu").animate({
+            left: "-200px"
+        }, {
+            duration: 300,
+            complete: function() {
+                menuOpened = false;
+            }
+        });
+    }
+}
